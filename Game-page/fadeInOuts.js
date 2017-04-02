4e6a4a7967d2efde8595c264c9ec4b969f1f76c5
edvinasba <edvinas.balciunass@gmail.com>
2017-04-02 17:06:10 +0300

    added animations (for functions used ofc!)

---
+++ Game-page/fadeInOuts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    // Hide, tada delay puse sec, tada daryt fadeIn
+    // Kodel? kad panaudot dar 3 funkcijas
+    $("body").hide().delay(500).fadeIn(300);
+    $(".gameListItem").delay(800).each(function(index){
+        $(this).delay(100*index).animate({
+            top:'-10px',
+            opacity: '0.5'
+        });
+    })
+    $(".gameListItem").delay(110).each(function(index){
+        $(this).delay(100*index).animate({
+            top:'10px',
+            opacity: '1.0'
+        });
+    })
+});
