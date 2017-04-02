3f88174e010e0829195a6aeb8b8332de7284eeee
edvinasba <edvinas.balciunass@gmail.com>
2017-04-02 22:02:38 +0300

    Changed game page transition from body to gamepage

---
+++ nrd-jquery/main/Game-page/fadeInOuts.js
@@ -0,0 +1,32 @@
+// .ready()
+// .hide()
+// .delay()
+// .fadeIn()
+// .each()
+// .animate()
+// .attr()
+// .html()
+$(document).ready(function(){
+    // Hide, tada delay puse sec, tada daryt fadeIn
+    // Kodel? kad panaudot dar 3 funkcijas
+    $(".gamePage").hide().delay(500).fadeIn(1000);
+    $(".gameListItem").delay(800).each(function(index){
+        $(this).delay(100*index).animate({
+            top:'-10px',
+            opacity: '0.5'
+        });
+    })
+    $(".gameListItem").delay(110).each(function(index){
+        $(this).delay(100*index).animate({
+            top:'10px',
+            opacity: '1.0'
+        });
+    })
+    // Find not so original game titles
+    $(".gameListItem").each(function(index){
+        $("h3:contains(game)").attr("badTitle","yes");
+        $("h3:contains(GAME)").attr("badTitle","yes");
+    });
+
+    $("#allGames .gameListItem h3[badTitle*='yes']").html('IM NOT ORIGINAL');
+});
---
+++ nrd-jquery/main/Game-page/toggleCategories.js
@@ -0,0 +1,12 @@
+// .hasClass()
+// .fadeIn()
+// .fadeOut()
+// .toggleClass()
+function toggleCategories(){
+	var categ = $("#categories");
+	if(categ.hasClass("categoriesHidden"))
+		categ.fadeIn(100);
+	else
+		categ.fadeOut(500);
+    categ.toggleClass("categoriesHidden");
+}
