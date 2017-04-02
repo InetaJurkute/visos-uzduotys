6e4c11b326fdde8f701077fd57de0a1901d44a57
edvinasba <edvinas.balciunass@gmail.com>
2017-04-02 17:07:54 +0300

    longer fadein

--- Game-page/fadeInOuts.js
+++ Game-page/fadeInOuts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     // Hide, tada delay puse sec, tada daryt fadeIn
     // Kodel? kad panaudot dar 3 funkcijas
-    $("body").hide().delay(500).fadeIn(300);
+    $("body").hide().delay(500).fadeIn(1000);
     $(".gameListItem").delay(800).each(function(index){
         $(this).delay(100*index).animate({
             top:'-10px',
