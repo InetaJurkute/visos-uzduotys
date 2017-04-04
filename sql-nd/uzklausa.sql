3750579482d4664cca3a34089413929814b8be42
Ineta Jurkutė <jurkute.ineta@gmail.com>
2017-04-05 00:10:32 +0300

    fix sql 1 task

--- sql-nd/uzklausa.sql
+++ sql-nd/uzklausa.sql
@@ -6,25 +6,34 @@
 -- USERIAI - ZAIDIMAIOWNED
 
 -- First part of homework
+-- Jeigu reikia
+-- Sukuriam db
+--CREATE DATABASE webGameProject;
+
+-- KATEGORIJOS - ZAIDIMAI
+-- USERIAI - ZAIDIMAIOWNED
+
+-- First part of homework
 CREATE TABLE Categories(
-	categoryID int,
+	categoryID int NOT NULL IDENTITY(1,1),
 	categoryName varchar(255) NOT NULL,		-- 1. e)    Not null constraint, lentelės kūrimo metu	1t.
 	minimumAge int,
 	PRIMARY KEY (categoryID)				-- 1. a)    Trys Primary key (1)
 );
 
 CREATE TABLE Games(
-	gameID int,
+	gameID int NOT NULL IDENTITY(1,1),
 	gameName varchar(255) NOT NULL,
 	releaseOn varchar(255),
-	category varchar(255) NOT NULL,
+	category int NOT NULL,
 	recommendedAge int default 0,
 	-- Unique identifier, automatically not null
-	PRIMARY KEY (gameID)					-- 1. a)    Trys Primary key (2)
+	PRIMARY KEY (gameID),					-- 1. a)    Trys Primary key (2)
+	FOREIGN KEY (category) references Categories(categoryID)
 );
 
 CREATE TABLE Users(
-	userID int,
+	userID int NOT NULL IDENTITY(1,1),
 	username varchar(255) NOT NULL,
 	passwd varchar(255) NOT NULL,
 	age int,
@@ -44,27 +53,60 @@ CREATE TABLE Ownerships(
 
 
 --2.a 
+insert into Categories (categoryName, minimumAge) values ('action', 12);
+insert into Categories (categoryName, minimumAge) values ('shooter', 14);
+insert into Categories (categoryName, minimumAge) values ('horror', 16);
 
-insert into Games(gameName,releaseOn,category,recommendedAge)values('Battlefield1','2016-10-21','shooter',18);
-insert into Games(gameName,releaseOn,category,recommendedAge)values('ResidenEvil7','2017-01-24','Scary',21);
+insert into Games (gameName, releaseOn, category, recommendedAge) values ('Battlefield1', '2016-10-21', 2, 18);
+insert into Games (gameName, releaseOn, category, recommendedAge) values ('ResidenEvil7', '2017-01-24', 3, 21);
+insert into Games (gameName, releaseOn, category, recommendedAge) values ('FinalFantasyXII', '2009-01-24', 1, 16);
+insert into Games (gameName, releaseOn, category, recommendedAge) values ('Call of duty', '2014-01-24', 2, 16);
 
-insert into Users(username,passwd,age)values('Zaibas','zaibiugs123',28);
-insert into Users(username,passwd,age)values('Pukis','kazkoks123', 35);
+insert into Users (username, passwd, age) values ('Zaibas', 'zaibiugs123', 28);
+insert into Users (username, passwd, age) values ('Pukis', 'kazkoks123', 35);
+insert into Users (username, passwd, age) values ('AwesomeMe', 'awsq789', 18);
+insert into Users (username, passwd, age) values ('YourBro', 'mance', 24);
+
+insert into Ownerships (gameID, userID) values (1, 1);
+insert into Ownerships (gameID, userID) values (2, 1);
+insert into Ownerships (gameID, userID) values (2, 2);
+insert into Ownerships (gameID, userID) values (4, 2);
+insert into Ownerships (gameID, userID) values (2, 3);
+insert into Ownerships (gameID, userID) values (3, 3);
+insert into Ownerships (gameID, userID) values (1, 4);
+insert into Ownerships (gameID, userID) values (3, 4);
+insert into Ownerships (gameID, userID) values (4, 4);
 
 --2.b
+select * from Games 
+where gameName='Battlefield' or recommendedAge=21;
 
-select *from Games where gameName='Battlefield' or recommendedAge=21;
-select*from Users where username='Zaibas' and age=28;
+select * from Users 
+where username='Zaibas' and age=28;
 
 --2.c
-select*from games where category in ('shooter','scary');
-select*from games where category>=16;
+select * from games 
+where category in ('shooter','horror');
+
+select * from games 
+where recommendedAge>=16;
+
 --2.d
-select users.username,Ownerships.orderDate from users left join ownerships on users.userID=ownerships=gameID order by users.username;
+select users.username,Ownerships.orderDate 
+from users 
+left join ownerships 
+on users.userID=ownerships.gameID 
+order by users.username;
 
 --3.
-select count(gameID),category from Game group by category;
-Select count(userID),username from users group by username order by count(userID) desc;
+select count(gameID),category 
+from Game 
+group by category;
+
+Select count(userID),username 
+from users 
+group by username 
+order by count(userID) desc;
 
 --4.
  Drop table Games;
