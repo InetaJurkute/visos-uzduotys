26d26dc52162ab4caaacd55eeec93cbb53a1af83
edvinasba <edvinas.balciunass@gmail.com>
2017-04-04 17:49:02 +0300

    Added first part of sql querry, needs testing

--- sql-nd/uzklausa.sql
+++ sql-nd/uzklausa.sql
@@ -1,51 +1,58 @@
+-- Jeigu reikia
 -- Sukuriam db
-CREATE DATABASE webGameProject;
+--CREATE DATABASE webGameProject;
 
 -- KATEGORIJOS - ZAIDIMAI
 -- USERIAI - ZAIDIMAIOWNED
 
--- Categories
+-- First part of homework
 CREATE TABLE Categories(
-	cateogryID int,
-	categoryName varchar(255) not null,
+	categoryID int,
+	categoryName varchar(255) NOT NULL,		-- 1. e)    Not null constraint, lentelės kūrimo metu	1t.
 	minimumAge int,
-	PRIMARY KEY (categoryID);
+	PRIMARY KEY (categoryID)				-- 1. a)    Trys Primary key (1)
 );
--- Sukuriam lentelę
+
 CREATE TABLE Games(
 	gameID int,
 	gameName varchar(255) NOT NULL,
 	releaseOn varchar(255),
-	category varchar(255),
+	category varchar(255) NOT NULL,
+	recommendedAge int default 0,
 	-- Unique identifier, automatically not null
-	PRIMARY KEY (gameID)
+	PRIMARY KEY (gameID)					-- 1. a)    Trys Primary key (2)
 );
--- People of our website
+
 CREATE TABLE Users(
 	userID int,
-	username varchar(255),
-	passwd varchar(255),
+	username varchar(255) NOT NULL,
+	passwd varchar(255) NOT NULL,
 	age int,
-	PRIMARY KEY (userID)
+	PRIMARY KEY (userID)					-- 1. a)    Trys Primary key (3)
 );
--- Games that certain users own
+ALTER TABLE Users
+	ADD CHECK (age > 0);
+
 CREATE TABLE Ownerships(
 	gameID int,
-	userID int
+	userID int,
+	orderDate date DEFAULT GETDATE(),				-- 1. d)    Default constraint
+	FOREIGN KEY (gameID) references Games(gameID),	-- 1. b)    Du Foreign Key (1)
+	FOREIGN KEY (userID) references Users(userID)	-- 1. b)    Du Foreign Key (2)
 );
 
 -- 1. Pateikite skriptą, kuris sukurtų trijų lentelių schemą pasirinkta tema. Kuriant schemą būtų panaudota:
--- 		a)    Trys Primary key                                                                                                                                        3t.
--- 		b)    Du Foreign Key                                                                                                                                                    2t.
--- 		c)    Check constraint, po lentelės sukūrimo                                                                                                        1t.
---		d)    Default constraint                                                                                                                                       1t.
--- 		e)    Not null constraint, lentelės kūrimo metu                                                                                                      1t.
+-- 		a)    Trys Primary key	3t. Yra
+-- 		b)    Du Foreign Key	2t. Yra
+-- 		c)    Check constraint, po lentelės sukūrimo	1t. Yra
+--		d)    Default constraint	1t. Yra
+-- 		e)    Not null constraint, lentelės kūrimo metu	1t. Primary key taip pat reiškia ne null, bet panaudojau
 -- 2. Sukurtai shemai pateikti užklausas:
--- 		a)    Pateikti insert sakinių, pradinių duomenų užpildymui                                                                                    3t.
--- 		b)    Užklausa su where sąlyga, kuri pademonstruotų AND, OR operatorių naudojimą                     2t.
--- 		c)     Užklausą, kuri demonstruotų <> ir IN naudojimą                                                                     2t.
--- 		d)    Užklausą, kuri parodytų kaip veikia bet kuri iš join operacijų                                                    2t.
--- 3. Pasidomėti GROUP BY užklausa, pateikti pvz. savo sukurtai schemai.                       5t.
+-- 		a)    Pateikti insert sakinių, pradinių duomenų užpildymui		3t.
+-- 		b)    Užklausa su where sąlyga, kuri pademonstruotų AND, OR operatorių naudojimą	2t.
+-- 		c)    Užklausą, kuri demonstruotų <> ir IN naudojimą	2t.
+-- 		d)    Užklausą, kuri parodytų kaip veikia bet kuri iš join operacijų	2t.
+-- 3. Pasidomėti GROUP BY užklausa, pateikti pvz. savo sukurtai schemai.	5t.
 -- 4. Pateikti sukurtų duomenų sunaikinimo skriptą.
 
 
