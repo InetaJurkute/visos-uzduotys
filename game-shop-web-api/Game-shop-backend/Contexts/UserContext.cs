c7ab8c6a464144931f981ad0e871e5133cea97a7
edvinasba <edvinas.balciunass@gmail.com>
2017-04-07 10:47:48 +0300

    User controller, context

---
+++ game-shop-web-api/Game-shop-backend/Contexts/UserContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using game_shop_web_api.Models;
+using game_shop_web_api.Contexts;
+
+namespace game_shop_web_api.Context
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base("GameShopDB")
+        {
+            //Database.SetInitializer(new GameDBInitializer());
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
